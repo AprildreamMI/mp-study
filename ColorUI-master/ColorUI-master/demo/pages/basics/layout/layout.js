5524e2824e76a17b2be13c16a7fdd971b0178584
AprildreamMI <magicwingzs@qq.com>
2019-07-28 09:24:22 +0800

    colorUi

---
+++ ColorUI-master/ColorUI-master/demo/pages/basics/layout/layout.js
@@ -0,0 +1,13 @@
+const app = getApp();
+Page({
+  data: {
+    CustomBar: app.globalData.CustomBar,
+    TabCur:0,
+    tabNav: ['Flex布局', 'Grid布局', '辅助布局']
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id
+    })
+  }
+})
